e5b4c625a86c8450a9c0776cbdbf1ca728541819
mesut <gelukkig.zijn.be@gmail.com>
2020-03-23 02:36:38 +0100

    update init.js

--- init.js
+++ init.js
@@ -1,17 +1,6 @@
 window.onload = () => {
     // debugger;
-    let divEl = document.getElementById('selected');
-    divEl.innerText = stringSorter.state.active;
-
-    let ulEl = document.createElement('UL');
-
-    for (let item of stringSorter.state[stringSorter.state.active]) {
-        let liEl = document.createElement('LI');
-        liEl.innerText = item;
-
-        ulEl.appendChild(liEl);
-    }
-    divEl.appendChild(ulEl);
+    stringSorter.renderActiveList();
 
     // log initial state
     log.push({
